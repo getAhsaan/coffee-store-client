f7dba2f5d32c47c08c6523c2964a3c8664141aaa
getahsaan <getahsaan@gamil.com>
2023-05-08 23:24:11 +0600

    google font added in this project

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddCoffee = () => {
+  return (
+    <div className="w-[1320px] h-[878px] bg-[#F4F3F0]">
+      <h2 className="font-rancho" >Add a fdgdsfgdfs coffee</h2>
+    </div>
+  );
+};
+
+export default AddCoffee;
