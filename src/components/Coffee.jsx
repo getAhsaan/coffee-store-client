ec32ff8bb9bebeb2d659a6fb65306925b09fd9f8
getahsaan <getahsaan@gamil.com>
2023-05-09 13:05:43 +0600

    delete coffee done

--- src/components/Coffee.jsx
+++ src/components/Coffee.jsx
@@ -1,9 +1,39 @@
 import React from "react";
 import { FaEye, FaTrash } from "react-icons/fa";
 import { HiPencil } from "react-icons/hi2";
+import Swal from "sweetalert2";
 
 const Coffee = ({ coffee }) => {
-  const { name, chef, price, taste, category, details, photo } = coffee;
+  const { _id, name, chef, price, taste, category, details, photo } = coffee;
+
+
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/coffee/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire("Deleted!", "Your coffee has been deleted.", "success");
+            }
+          });
+        // console.log("delete confirmed");
+      }
+    });
+  };
+
   return (
     <div>
       <div className="bg-[#F5F4F1] rounded-xl flex justify-between items-center p-4">
@@ -31,7 +61,10 @@ const Coffee = ({ coffee }) => {
           <button className="bg-[#3C393B] hover:btn-secondary p-2 rounded-xl m-1">
             <HiPencil className="text-3xl text-white"></HiPencil>
           </button>
-          <button className="bg-[#EA4744] hover:btn-primary p-2 rounded-xl m-1">
+          <button
+            onClick={() => handleDelete(_id)}
+            className="bg-[#EA4744] hover:btn-primary p-2 rounded-xl m-1"
+          >
             <FaTrash className="text-3xl text-white"></FaTrash>
           </button>
         </div>
